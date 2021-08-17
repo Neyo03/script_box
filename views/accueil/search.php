295ce731a27f2dd7f7bc7422a715b0941aed351e
Neyo03 <wagnerneo03@gmail.com>
2021-08-17 21:54:03 +0200

    master

--- views/accueil/search.php
+++ views/accueil/search.php
@@ -3,7 +3,7 @@
     <?php foreach ($listeSearch as $search ):?>
         <a href="/script_box/commentaire/showPost/<?= $search->getIdCommentaire();?>">
             <div class="blocSearch">
-                <p><?= $search->getPseudo($search->getIdCommentaire())?></p>
+                <p><?= $search->afficherPseudo($search->getIdCommentaire())?></p>
                 <h3><?= $search->getTitre()?></h3>
                 <p><?= $search->getContenu()?></p>
             </div>
